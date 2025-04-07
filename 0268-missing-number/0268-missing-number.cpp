81d04dd51787c1fa587cf1eac937568a694afb23
Hemant Dubey <hemantd107@gmail.com>
2025-04-07 15:36:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int ans=0;
-        for(int i=1;i<=nums.size();i++)
+        int xor1=0,xor2=0;
+        int n=nums.size();
+        for(int i=0;i<n-1;i++)
         {
-            ans=ans^i;
+            xor2=xor2^nums[i];
+            xor1=xor1^(i+1);
         }
-        for(int i=0;i<nums.size();i++)
-        {
-            ans=nums[i]^ans;
-        }
-        return ans;
+        xor1=xor1^n;
+        return (xor1^xor2);
+
     }
 };
