f2db4fdfd456e8343655f16ec076bac95d937349
Hemant Dubey <hemantd107@gmail.com>
2025-05-02 17:40:04 +0530

    Time: 0 ms (100%), Space: 71.9 MB (18.46%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -6,20 +6,8 @@ public:
         for(int i=0;i<nums.size();i++)
         {
             sum+=nums[i];
-            /*int sum=0;
-            for(int j=i;j<nums.size();j++)
-            {
-                sum+=nums[j];
-                max_sum=max(sum,max_sum);
-            }*/
-            if(sum>maxi)
-            {
-                maxi=sum;
-            }
-            if(sum<0)
-            {
-                sum=0;
-            }
+            maxi=max(sum,maxi);
+            if(sum<0) sum=0;
         }
         return maxi;
     }
