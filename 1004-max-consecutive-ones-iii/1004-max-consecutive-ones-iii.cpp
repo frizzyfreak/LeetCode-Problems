3d0f814a7ce994e6cd1f33484db7ecf95ceff0c2
Hemant Dubey <hemantd107@gmail.com>
2025-06-16 01:01:30 +0530

    Time: 0 ms (100%), Space: 59.3 MB (25.81%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -4,16 +4,18 @@ public:
         int l=0,r=0;
         int maxlen=0;
         int zeros=0;
-        while(r<nums.size())
-        {
+        while(r<nums.size()){
             if(nums[r]==0) zeros++;
-            while(zeros>k){
-                if(nums[l]==0){
-                    zeros--;
-                }
+            if(zeros>k) 
+            {
+                if(nums[l]==0) zeros--;
                 l++;
             }
-            maxlen=max(maxlen,r-l+1);
+            if(zeros<=k)
+            {
+                int len=r-l+1;
+                maxlen=max(maxlen,len);
+            }
             r++;
         }
         return maxlen;
