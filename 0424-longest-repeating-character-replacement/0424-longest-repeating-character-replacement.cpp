84e95cd89bb96757b271b5c8acc7d1a4d341bf89
Hemant Dubey <hemantd107@gmail.com>
2025-07-01 15:06:08 +0530

    Time: 3 ms (72.38%), Space: 10.8 MB (70.46%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int max_f,max_len=0;
-        for(int i=0;i<s.size();i++)
-        {
-            if(s.size()-i<=max_len) break;
-            int hash[26] = {0};
-            max_f=0;
-            for(int j=i;j<s.size();j++){
-                hash[s[j]-'A']++;
-                max_f=max(max_f,hash[s[j]-'A']);
-                int changes=(j-i+1)-max_f;
-                if(changes<=k){
-                    max_len=max(max_len,j-i+1);
+        int l=0,r=0,maxlen=0,maxf=0;
+        int hash[26]={0};
+        while(r<s.size()){
+            hash[s[r]-'A']++;
+            maxf=max(maxf,hash[s[r]-'A']);
+            while((r-l+1)-maxf>k)
+            {
+                hash[s[l]-'A']--;
+                maxf=0;
+                for(int i=0;i<26;i++)
+                {
+                    maxf=max(maxf,hash[i]);
                 }
-                else break;
+                l++;
             }
+            if((r-l+1)-maxf<=k){
+                maxlen=max(maxlen,r-l+1);
+            }
+        r++;
         }
-        return max_len;
+    return maxlen;
     }
 };
