5d263291486bed4d96294515c84864f327dbc36a
Hemant Dubey <hemantd107@gmail.com>
2025-07-09 23:50:18 +0530

    Time: 1 ms (23.08%), Space: 46.3 MB (47.16%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     int maxScore(vector<int>& cardPoints, int k) {
         int lsum=0,rsum=0,maxSum=0;
-        for(int i=0;i<k-1;i++)
+        for(int i=0;i<k;i++)
         {
             lsum+=cardPoints[i];
             maxSum=lsum;
         }
         int r_ind=cardPoints.size()-1;
-        for(int i=k-1;i>0;i--)
+        for(int i=k-1;i>=0;i--)
         {
+            lsum-=cardPoints[i];
             rsum= rsum+cardPoints[r_ind];
             r_ind--;
-            lsum-=cardPoints[i];
             maxSum=max(maxSum, lsum+rsum);
         }
         return maxSum;
