6c52ec40c030eda51875b9fe5e2aa25d8b9f3250
Hemant Dubey <hemantd107@gmail.com>
2025-09-07 00:55:45 +0530

    Time: 8 ms (15.27%), Space: 23.8 MB (10.97%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,24 +1,13 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        int n=s.size();
-        //reverse krdo poore string ko
-        reverse(s.begin(),s.end());
-        int i=0;
-        int l=0,r=0;
-        while(i<n){
-            while(s[i]!=' ')
-            s[r++] = s[i++];
-
-            if(l<r){
-                reverse(s.begin()+l,s.begin()+r);
-                s[r]=' ';
-                r++;
-                l=r;
-            }
-            i++;
+        stringstream ss(s);
+        string token = "";
+        string res = "";
+        while(ss >> token){
+            res = token+" "+res;
         }
-        s= s.substr(0, r-1);
-        return s;
+        res.pop_back();
+        return res;
     }
 };
