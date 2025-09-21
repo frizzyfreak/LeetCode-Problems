b62f79d2fe7c04691f33e7feedbfa2f6f2f7d3fd
Hemant Dubey <hemantd107@gmail.com>
2025-09-22 00:36:21 +0530

    Time: 109 ms (33.64%), Space: 171.9 MB (50.17%) - LeetHub

---
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int longestPalindrome(vector<string>& words) {
+        unordered_map<string , int>mp;
+
+        for(int i=0;i<words.size();i++){
+            mp[words[i]]++;
+        }
+
+        bool centreused = false;
+
+        int res=0;
+
+        for(string& word:words){
+            string rev=word;
+            reverse(begin(rev), end(rev));
+
+            if(rev!=word){ 
+                if(mp[rev]>0 && mp[word]>0)
+                { // .ab      ba.
+                mp[word]--;
+                mp[rev]--;
+                res+=4;
+                }
+            }
+            else{
+                if(rev==word && mp[word]>=2){// for cases like [aa,bb]
+                    mp[word]-=2;
+                    res+=4;
+                }
+                else if(mp[word]==1 && centreused==false){  //for the middle word || can fit only one string in mid
+                    mp[word]--;
+                    res+=2;
+                    centreused=true;
+                }
+            }
+        }
+        return res;
+    }
+};
