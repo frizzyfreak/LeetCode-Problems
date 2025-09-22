c1379e97f5d5b4529a7e38befd14e8d8290e93c7
Hemant Dubey <hemantd107@gmail.com>
2025-09-23 02:29:48 +0530

    Time: 0 ms (100%), Space: 8.7 MB (18.56%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,12 +1,9 @@
 class Solution {
 public:
-    int f(int i,vector<int>&dp){
-        //base condn
+    int f(int i, vector<int>&dp){
         if(i==0) return 1;
         if(i<0) return 0;
-        //dp condn
         if(dp[i]!=-1) return dp[i];
-        //take not take
         int take1=f(i-1,dp);
         int take2=f(i-2,dp);
         return dp[i]=take1+take2;
