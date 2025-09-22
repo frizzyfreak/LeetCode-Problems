a52b51dbe0a9d28ecd8fb70e54d5aa2d91bd3c6a
Hemant Dubey <hemantd107@gmail.com>
2025-09-23 00:31:37 +0530

    Time: 3 ms (88.42%), Space: 71.4 MB (68.15%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
-int nicesubarr(vector<int>nums, int goal)
-{
-    int l=0,r=0,presum=0,count=0;
-    while(r<nums.size())
-    {
-        presum+=nums[r]%2;
-        while(presum>goal && l<=r)
-        {
-            presum-=nums[l]%2;
-            l++;
+    int nicesubarr(vector<int>&nums, int goal){
+        int l=0;
+        int n=nums.size();
+        int r=0;
+        int count=0;
+        int presum=0;
+        while(r<n){
+            presum+=nums[r]%2;
+            while(presum>goal && l<=r){
+                presum-=nums[l]%2;
+                l++;
+            }
+            count+=r-l+1;
+            r++;
         }
-        count+=r-l+1;
-        r++;
+        return count;
     }
-    return count;
-}
+
     int numberOfSubarrays(vector<int>& nums, int k) {
         if(k<0) return 0;
         return nicesubarr(nums,k)-nicesubarr(nums,k-1);
